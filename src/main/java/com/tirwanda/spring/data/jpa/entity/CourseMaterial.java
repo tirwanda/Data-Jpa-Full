1776bd36ec365f45215e4ae9d35326fe6c01f986
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-27 20:48:42 +0700

    Create Entity Course and Course Material

---
+++ src/main/java/com/tirwanda/spring/data/jpa/entity/CourseMaterial.java
@@ -0,0 +1,38 @@
+package com.tirwanda.spring.data.jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseMaterial {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_material_sequence",
+            sequenceName = "course_material_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_material_sequence"
+    )
+    private Long courseMaterialId;
+    private String url;
+
+    // Declaration for One to One Relationship
+    @OneToOne
+    @JoinColumn(
+            name = "course_id",
+            referencedColumnName = "courseId"
+    )
+    private Course course;
+
+}
